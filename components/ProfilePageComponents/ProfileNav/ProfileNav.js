7891aad012769555c470c57eeee5ab3571fcb8fc
Mavinxukr <mavinx.office@gmail.com>
2019-12-02 16:51:15 +0300

    add navigation to profile page

--- components/ProfilePageComponents/ProfileNav/ProfileNav.js
+++ components/ProfilePageComponents/ProfileNav/ProfileNav.js
@@ -7,30 +7,34 @@ import ProfileOrder from '../ProfileOrder/ProfileOrder';
 import ProfileData from '../ProfileData/ProfileData';
 import ProfileDataEdit from '../ProfileDataEdit/ProfileDataEdit';
 import ProfileFavourite from '../ProfileFavourite/ProfileFavourite';
+import Link from 'next/link';
 import Styles from './ProfileNav.module.scss';
 import './ProfileNav.scss';
 
 const ProfileNav = () => {
   const [openEdit, setOpenEdit] = useState(false);
+  const [valueForCrumb, setValueForCrumb] = useState('Мои данные');
 
   return (
     <>
-      <ProfileBreadCrumbs />
+      <ProfileBreadCrumbs valueForCrumb={valueForCrumb} />
       <div className={Styles.ProfileNav__Content}>
         <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="order" />
         <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="favorites" />
         <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="bonus" />
         <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="seen" />
         <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="links" />
-        <input type="radio" className={Styles.ProfileNav__Field} name="switcher" id="data" />
+        <input defaultChecked type="radio" className={Styles.ProfileNav__Field} name="switcher" id="data" />
         <nav className={Styles.ProfileNav__Nav}>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="order" type="button">Заказы</label>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="favorites" type="button">Избранные</label>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="bonus" type="button">Бонусы</label>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="seen" type="button">Просмотренные</label>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="links" type="button">Рассылки</label>
-          <label className={Styles.ProfileNav__Switcher} htmlFor="data" type="button">Мои данные</label>
-          <button className={Styles.ProfileNav__ButtonExit} type="button">Выйти</button>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="order" type="button">Заказы</label>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="favorites" type="button">Избранные</label>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="bonus" type="button">Бонусы</label>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="seen" type="button">Просмотренные</label>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="links" type="button">Рассылки</label>
+          <label onClick={e => setValueForCrumb(e.target.textContent)} className={Styles.ProfileNav__Switcher} htmlFor="data" type="button">Мои данные</label>
+          <Link href="/">
+            <a className={Styles.ProfileNav__ButtonExit}>Выйти</a>
+          </Link>
         </nav>
         <div id="order" className={Styles.ProfileNav__Item}>
           <ProfileOrder />
