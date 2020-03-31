4c88bc23944f9d6f8abd0c125f4479ef75f07ff9
Mavinxukr <mavinx.office@gmail.com>
2020-03-31 14:14:04 +0300

    fix cart page

--- components/Wrappers/Cart/Cart.jsx
+++ components/Wrappers/Cart/Cart.jsx
@@ -203,7 +203,13 @@ const Cart = () => {
                   К сожалению в корзине ничего нет, возможно вы посмотрите наши
                   новинки?
                 </h4>
-                <Link href="/">
+                <Link href={{
+                  pathname: '/Products',
+                  query: {
+                    sort_date: 'desc',
+                  },
+                }}
+                >
                   <Button
                     href
                     title="Посмотреть новинки"
@@ -266,6 +272,7 @@ CartItem.propTypes = {
       id: PropTypes.number,
       img_link: PropTypes.string,
       count: PropTypes.number,
+      new_price: PropTypes.number,
     }),
     count: PropTypes.number,
     color: PropTypes.shape({
