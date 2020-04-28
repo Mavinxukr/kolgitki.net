fe63601c82b3ccb48238d7e58939ddaf78de3137
Mavinxukr <mavinx.office@gmail.com>
2020-04-28 19:02:50 +0300

    finish sprint 6

--- components/Layout/FormWrapper/FormWrapper.jsx
+++ components/Layout/FormWrapper/FormWrapper.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import Head from 'next/head';
+import cx from 'classnames';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import { withResponse } from '../../hoc/withResponse';
 import styles from './FormWrapper.scss';
 
-const FormWrapper = ({ children }) => (
-  <>
-    <Head>
-      <title>Home</title>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {process.env.NODE_ENV !== 'production' && (
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href={`/_next/static/css/styles.chunk.css?v=${Date.now()}`}
-        />
-      )}
-      <link rel="stylesheet" href="/uikit/uikit.css" />
-      <script src="/uikit/uikit.js" />
-    </Head>
-    <div className={styles.formWrapper}>
-      {children}
-    </div>
-  </>
-);
+const FormWrapper = ({ children, isMobileScreen }) => {
+  const router = useRouter();
 
-FormWrapper.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object,
-  ]),
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {process.env.NODE_ENV !== 'production' && (
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href={`/_next/static/css/styles.chunk.css?v=${Date.now()}`}
+          />
+        )}
+        <link rel="stylesheet" href="/uikit/uikit.css" />
+        <script src="/uikit/uikit.js" />
+      </Head>
+      <div
+        className={cx(styles.formWrapper, {
+          [styles.formWrapperForRegistration]:
+            router.pathname === '/registration' && isMobileScreen,
+        })}
+      >
+        {children}
+      </div>
+    </>
+  );
 };
 
-export default FormWrapper;
+FormWrapper.propTypes = {
+  children: PropTypes.node,
+  isMobileScreen: PropTypes.bool,
+};
+
+export default withResponse(FormWrapper);
