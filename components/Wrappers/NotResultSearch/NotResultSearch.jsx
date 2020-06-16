814c12bb957b3f45f6d1d7fb4de41626d721f9a9
SamborVadym <samborvadym@gmail.com>
2020-06-16 16:18:11 +0300

    added last language

--- components/Wrappers/NotResultSearch/NotResultSearch.jsx
+++ components/Wrappers/NotResultSearch/NotResultSearch.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { cookies } from '../../../utils/getCookies';
-import { setFiltersInCookies } from '../../../utils/helpers';
+import { setFiltersInCookies, parseText } from '../../../utils/helpers';
 import MainLayout from '../../Layout/Global/Global';
 import ButtonRoute from '../../Layout/ButtonRoute/ButtonRoute';
 import Button from '../../Layout/Button/Button';
@@ -10,7 +10,13 @@ import styles from './NotResultSearch.scss';
 const NotResultSearch = () => (
   <MainLayout>
     <div className={styles.noResultWrapper}>
-      <p>нет результатов по данному запросу</p>
+      <p>
+        {parseText(
+          cookies,
+          'нет результатов по данному запросу',
+          'немає результатів по даному запиту',
+        )}
+      </p>
       <ButtonRoute classNameWrapper={styles.buttonRouteWrapper} />
       <Link
         href={{
@@ -21,6 +27,7 @@ const NotResultSearch = () => (
       >
         <Button
           title="посмотреть новинки"
+          titleUa="переглянути новинки"
           viewType="white"
           href
           classNameWrapper={styles.buttonRouteWrapper}
--- components/Wrappers/Thank/Thank.jsx
+++ components/Wrappers/Thank/Thank.jsx
@@ -11,6 +11,7 @@ import ButtonRoute from '../../Layout/ButtonRoute/ButtonRoute';
 import Loader from '../../Loader/Loader';
 import styles from './Thank.scss';
 import { cookies } from '../../../utils/getCookies';
+import { parseText } from '../../../utils/helpers';
 
 const Thank = () => {
   const orders = useSelector(ordersDataSelector);
@@ -32,16 +33,22 @@ const Thank = () => {
   return (
     <MainLayout>
       <div className={styles.thank}>
-        <h2 className={styles.title}>Спасибо за покупку!</h2>
+        <h2 className={styles.title}>
+          {parseText(cookies, 'Спасибо за покупку!', 'Дякуємо за покупку!')}
+        </h2>
         <p className={styles.order}>
-          Номер вашего заказа:{' '}
+          {parseText(cookies, 'Номер вашего заказа', 'Номер вашого замовлення')}
+          :{' '}
           <span className={styles.orderNumber}>
             {orders[0] ? orders[0].id : cookies.get('idOrder')}
           </span>
         </p>
         <p className={styles.desc}>
-          На вашу почту и телефон мы выслали реквизиты и подробную информацию о
-          заказе
+          {parseText(
+            cookies,
+            'На вашу почту и телефон мы выслали реквизиты и подробную информацию о заказе',
+            'На вашу пошту та телефон ми вислали реквізити та детальну інформацію щодо замовлення',
+          )}
         </p>
         <ButtonRoute classNameWrapper={styles.routeWrapper} />
       </div>
