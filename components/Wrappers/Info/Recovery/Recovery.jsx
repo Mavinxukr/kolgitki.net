de23910e1885592682cb5e7ebd2164ef9471de22
SamborVadym <samborvadym@gmail.com>
2020-06-16 16:31:09 +0300

    added language Recovery

--- components/Wrappers/Info/Recovery/Recovery.jsx
+++ components/Wrappers/Info/Recovery/Recovery.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cookies } from '../../../../utils/getCookies';
+import { parseText } from '../../../../utils/helpers';
 import styles from './Recovery.scss';
 
 const Recovery = ({ exchangeData }) => (
@@ -9,17 +11,25 @@ const Recovery = ({ exchangeData }) => (
         <h3>{item.name}</h3>
         <div className={styles.mainIfo}>
           <div className={styles.iconBlock}>
-            <img className={styles.recoveryImage} src={item.image_link} alt={item.image_link} />
+            <img
+              className={styles.recoveryImage}
+              src={item.image_link}
+              alt={item.image_link}
+            />
           </div>
           <p className={styles.mainInfoDesc}>
-            Мы делаем все для того, чтобы ваш опыт онлайн-шопинга был
-            максимально приятным, и разработали максимально простую и удобную
-            процедуру возврата.
+            {parseText(
+              cookies,
+              'Мы делаем все для того, чтобы ваш опыт онлайн-шопинга был максимально приятным, и разработали максимально простую и удобную процедуру возврата.',
+              'Ми робимо все для того, щоб ваш досвід онлайн-шопінгу був максимально приємним, і розробили максимально просту і зручну процедуру повернення.',
+            )}
           </p>
         </div>
         <div
           className={styles.detailsTitle}
-          dangerouslySetInnerHTML={{ __html: item.description }}
+          dangerouslySetInnerHTML={{
+            __html: parseText(cookies, item.description, item.description_ua),
+          }}
         />
       </div>
     ))}
