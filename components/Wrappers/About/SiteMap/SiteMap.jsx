adba894cf180064b940ca87fe02524df60691202
SamborVadym <samborvadym@gmail.com>
2020-08-12 13:26:50 +0300

    sitMap

--- components/Wrappers/About/SiteMap/SiteMap.jsx
+++ components/Wrappers/About/SiteMap/SiteMap.jsx
@@ -65,15 +65,23 @@ const SiteMap = ({ isMobileScreenForSiteMap }) => {
   return (
     <div className={styles.siteMap}>
       {createArrForSiteMap(categories).map((item, index, arr) => {
-        const classNameForHeader = cx(styles.itemHeader, {
-          [styles.itemHeaderWithoutHeight]:
-            isMobileScreenForSiteMap && !item.mainTitle,
-        });
+        const classNameForHeader = cx(
+          styles.itemHeader,
+          {
+            [styles.itemHeaderWithoutHeight]:
+              isMobileScreenForSiteMap && !item.mainTitle,
+          },
+          {
+            [styles.noTitle]: !item.mainTitle,
+          },
+        );
 
         return (
-          <div className={cx(styles.item, {
-            [styles.itemLeftBorder]: item.mainTitle,
-          })}>
+          <div
+            className={cx(styles.item, {
+              [styles.itemLeftBorder]: item.mainTitle,
+            })}
+          >
             <div className={classNameForHeader}>
               {item.mainTitle && (
                 <h3 className={styles.titleMain}>
@@ -85,9 +93,11 @@ const SiteMap = ({ isMobileScreenForSiteMap }) => {
                 </h3>
               )}
             </div>
-            <div className={cx(styles.lists, {
-              [styles.listsWithoutBorder]: arr[index + 1]?.mainTitle
-            })}>
+            <div
+              className={cx(styles.lists, {
+                [styles.listsWithoutBorder]: arr[index + 1]?.mainTitle,
+              })}
+            >
               <div>
                 {item.title && (
                   <a
