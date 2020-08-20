d78bd2e3dd4cad72e7594ee3093fcbcb269a136d
SamborVadym <samborvadym@gmail.com>
2020-08-20 13:52:57 +0300

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
+import { DefaultSeo } from 'next-seo';
 import createStore from '../redux/store';
 
 const MyApp = ({ Component, pageProps, store }) => (
   <Provider store={store}>
+    <DefaultSeo
+      openGraph={{
+        type: 'website',
+        locale: 'ru-UA',
+        url: 'https://dev.kolgotki.ml/',
+        site_name: ' Kolgotki',
+      }}
+    />
     <Component {...pageProps} />
   </Provider>
 );
