e9b6efe1113d2ce647dbf791180b1bffdd6a7510
CDenis <denclub2013@gmail.com>
2020-11-04 10:16:08 +0200

    added new url

--- enums/api.js
+++ enums/api.js
@@ -1,2 +1,2 @@
-export const API_DOMAIN = 'https://kolgotki.mavinx.com/api/';
+export const API_DOMAIN = 'https://backkolgot.mavinx.com/api/';
 export const API_NEW_POST = 'https://api.novaposhta.ua/v2.0/json/';
