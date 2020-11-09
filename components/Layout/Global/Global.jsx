afc91b8aa35655e99c274a1d15f96bfe1c7b61f9
CDenis <denclub2013@gmail.com>
2020-11-09 10:22:21 +0200

    test

--- components/Layout/Global/Global.jsx
+++ components/Layout/Global/Global.jsx
@@ -83,7 +83,6 @@ const Global = ({ children, seo = {}, openPopup }) => {
         <meta name="keywords" content={seo.seo_keywords || seoData.meta_keywords} />
         <meta name="title" content={seo.seo_title || seoData.meta_title} />
         <link rel="stylesheet" href="/uikit/uikit.css" />
-        {seo.seo_canonical && <link rel="canonical" href={seo.seo_canonical} />}
         {process.env.NODE_ENV !== 'production' && (
           <link
             rel="stylesheet"
@@ -91,6 +90,7 @@ const Global = ({ children, seo = {}, openPopup }) => {
             href={`/_next/static/css/styles.chunk.css?v=${Date.now()}`}
           />
         )}
+        {seo.seo_canonical && <link rel="canonical" href={seo.seo_canonical} />}
         <script src="/uikit/uikit.js" />
         {(router.pathname === '/order' && (
           <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb8D7DDVkbXbN03KeDk0TFmBpK24NcQjg&libraries=places" />
