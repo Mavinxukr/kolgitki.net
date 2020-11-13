c4447e1a91a5b03183ae6d60afc4dfdde0101cfe
CDenis <denclub2013@gmail.com>
2020-11-13 13:34:18 +0200

    fixed config

--- next.config.js
+++ next.config.js
@@ -11,12 +11,12 @@ module.exports = withCSS(
     webpack(config) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
-        },
+        // use: {
+        //   loader: 'url-loader',
+        //   options: {
+        //     limit: 100000,
+        //   },
+        // },
       });
 
       return config;
