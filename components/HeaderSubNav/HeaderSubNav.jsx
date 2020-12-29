54d1bf2815a8430ec5032351a0cf4498f8acc4a4
SamborVadym <samborvadym@gmail.com>
2020-12-29 12:34:17 +0200

    fixe filters

--- components/HeaderSubNav/HeaderSubNav.jsx
+++ components/HeaderSubNav/HeaderSubNav.jsx
@@ -26,12 +26,7 @@ const HeaderSubNav = ({
       categories: [],
     });
     isHover(false);
-    router.push(
-      '/Products',
-      `/Products/${createCleanUrl(cookies).join(
-        '/',
-      )}`,
-    );
+    router.push('/Products', `/Products/${createCleanUrl(cookies).join('/')}`);
   };
 
   return (
@@ -55,7 +50,11 @@ const HeaderSubNav = ({
                         setSubNavItem({
                           id: item.id,
                           name: item.slug,
-                          categoryName: parseText(cookies, item.name, item.name_ua),
+                          categoryName: parseText(
+                            cookies,
+                            item.name,
+                            item.name_ua,
+                          ),
                         });
                       }}
                     >
@@ -80,110 +79,127 @@ const HeaderSubNav = ({
                         }}
                       >
                         {item.name}
-                        <img className={styles.firstImg} src='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyLjg1NCA5LjY0M0w4LjM1NyA1LjE0OGEuNTA0LjUwNCAwIDEwLS43MTUuNzEzTDExLjc4MiAxMGwtNC4xNCA0LjEzOWEuNTA0LjUwNCAwIDEwLjcxNS43MTNsNC40OTctNC40OTVhLjUxLjUxIDAgMDAwLS43MTR6IiBmaWxsPSIjMjEyQjM2Ii8+PC9zdmc+' />
+                        <img
+                          className={styles.firstImg}
+                          alt="image"
+                          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyLjg1NCA5LjY0M0w4LjM1NyA1LjE0OGEuNTA0LjUwNCAwIDEwLS43MTUuNzEzTDExLjc4MiAxMGwtNC4xNCA0LjEzOWEuNTA0LjUwNCAwIDEwLjcxNS43MTNsNC40OTctNC40OTVhLjUxLjUxIDAgMDAwLS43MTR6IiBmaWxsPSIjMjEyQjM2Ii8+PC9zdmc+"
+                        />
                       </a>
                       <ul className={classNameForList}>
-                        {item.subcategory.map(itemChild => (
-                          <li className={styles.subProductsItem} key={itemChild.id}>
-                            <div
-                              onMouseOver={() => {
-                                setSrc(itemChild.image_link);
-                                setSubNavItemChild({
-                                  id: itemChild.id,
-                                  name: itemChild.slug,
-                                  categoryName: parseText(
-                                    cookies,
-                                    itemChild.name,
-                                    itemChild.name_ua,
-                                  ),
-                                });
-                              }}
-                              className={styles.subProductsInfo}
-                            >
-                              <p className={styles.subProductsInfoText}>
-                                <a
-                                  href="/"
-                                  onFocus={() => setSrc(itemChild.image_link)}
-                                  onClick={(e) => {
-                                    e.preventDefault();
-                                    setFiltersInCookies(cookies, {
-                                      categories: [
-                                        activeMenu,
-                                        subNavItem,
-                                        subNavItemChild,
-                                      ],
+                        {item.subcategory.map((itemChild, indexChild) => (
+                          <>
+                            {indexChild < 6 && (
+                              <li
+                                className={styles.subProductsItem}
+                                key={itemChild.id}
+                              >
+                                <div
+                                  onMouseOver={() => {
+                                    setSrc(itemChild.image_link);
+                                    setSubNavItemChild({
+                                      id: itemChild.id,
+                                      name: itemChild.slug,
+                                      categoryName: parseText(
+                                        cookies,
+                                        itemChild.name,
+                                        itemChild.name_ua,
+                                      ),
                                     });
-                                    router.push(
-                                      '/Products',
-                                      `/Products/${createCleanUrl(cookies).join(
-                                        '/',
-                                      )}`,
-                                    );
-                                    if (
-                                      router.pathname.indexOf('/Products') !== -1
-                                    ) {
-                                      isHover(false);
-                                    }
                                   }}
+                                  className={styles.subProductsInfo}
                                 >
-                                  {itemChild.name}
-                                </a>
-                                {itemChild.subcategory.length > 0 && (
-                                  <img src='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyLjg1NCA5LjY0M0w4LjM1NyA1LjE0OGEuNTA0LjUwNCAwIDEwLS43MTUuNzEzTDExLjc4MiAxMGwtNC4xNCA0LjEzOWEuNTA0LjUwNCAwIDEwLjcxNS43MTNsNC40OTctNC40OTVhLjUxLjUxIDAgMDAwLS43MTR6IiBmaWxsPSIjMjEyQjM2Ii8+PC9zdmc+' />
-                                )}
-                              </p>
-                            </div>
-                            <ul className={styles.subChildList}>
-                              {itemChild.subcategory.map(itemSubChild => (
-                                <li
-                                  className={styles.subChildItem}
-                                  key={itemSubChild.id}
-                                >
-                                  <a
-                                    onClick={(e) => {
-                                      e.preventDefault();
-                                      setFiltersInCookies(cookies, {
-                                        categories: [
-                                          activeMenu,
-                                          subNavItem,
-                                          subNavItemChild,
-                                          subNavItemSubChild,
-                                        ],
-                                      });
-                                      router.push(
-                                        '/Products',
-                                        `/Products/${createCleanUrl(cookies).join(
-                                          '/',
-                                        )}`,
-                                      );
-                                      if (
-                                        router.pathname.indexOf('/Products') !== -1
-                                      ) {
-                                        isHover(false);
+                                  <p className={styles.subProductsInfoText}>
+                                    <a
+                                      href="/"
+                                      onFocus={() => setSrc(itemChild.image_link)
                                       }
-                                    }}
-                                    onMouseOver={() => {
-                                      setSrc(itemSubChild.image_link);
-                                      setSubNavItemSubChild({
-                                        id: itemSubChild.id,
-                                        name: itemSubChild.slug,
-                                        categoryName: parseText(
-                                          cookies,
-                                          itemSubChild.name,
-                                          itemSubChild.name_ua,
-                                        ),
-                                      });
-                                    }}
-                                    onFocus={() => setSrc(itemSubChild.image_link)}
-                                    className={styles.subChildLink}
-                                    href="/"
-                                  >
-                                    {itemSubChild.name}
-                                  </a>
-                                </li>
-                              ))}
-                            </ul>
-                          </li>
+                                      onClick={(e) => {
+                                        e.preventDefault();
+                                        setFiltersInCookies(cookies, {
+                                          categories: [
+                                            activeMenu,
+                                            subNavItem,
+                                            subNavItemChild,
+                                          ],
+                                        });
+                                        router.push(
+                                          '/Products',
+                                          `/Products/${createCleanUrl(
+                                            cookies,
+                                          ).join('/')}`,
+                                        );
+                                        if (
+                                          router.pathname.indexOf(
+                                            '/Products',
+                                          ) !== -1
+                                        ) {
+                                          isHover(false);
+                                        }
+                                      }}
+                                    >
+                                      {itemChild.name}
+                                    </a>
+                                    {itemChild.subcategory.length > 0 && (
+                                      <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyLjg1NCA5LjY0M0w4LjM1NyA1LjE0OGEuNTA0LjUwNCAwIDEwLS43MTUuNzEzTDExLjc4MiAxMGwtNC4xNCA0LjEzOWEuNTA0LjUwNCAwIDEwLjcxNS43MTNsNC40OTctNC40OTVhLjUxLjUxIDAgMDAwLS43MTR6IiBmaWxsPSIjMjEyQjM2Ii8+PC9zdmc+" />
+                                    )}
+                                  </p>
+                                </div>
+                                <ul className={styles.subChildList}>
+                                  {itemChild.subcategory.map(itemSubChild => (
+                                    <li
+                                      className={styles.subChildItem}
+                                      key={itemSubChild.id}
+                                    >
+                                      <a
+                                        onClick={(e) => {
+                                          e.preventDefault();
+                                          setFiltersInCookies(cookies, {
+                                            categories: [
+                                              activeMenu,
+                                              subNavItem,
+                                              subNavItemChild,
+                                              subNavItemSubChild,
+                                            ],
+                                          });
+                                          router.push(
+                                            '/Products',
+                                            `/Products/${createCleanUrl(
+                                              cookies,
+                                            ).join('/')}`,
+                                          );
+                                          if (
+                                            router.pathname.indexOf(
+                                              '/Products',
+                                            ) !== -1
+                                          ) {
+                                            isHover(false);
+                                          }
+                                        }}
+                                        onMouseOver={() => {
+                                          setSrc(itemSubChild.image_link);
+                                          setSubNavItemSubChild({
+                                            id: itemSubChild.id,
+                                            name: itemSubChild.slug,
+                                            categoryName: parseText(
+                                              cookies,
+                                              itemSubChild.name,
+                                              itemSubChild.name_ua,
+                                            ),
+                                          });
+                                        }}
+                                        onFocus={() => setSrc(itemSubChild.image_link)
+                                        }
+                                        className={styles.subChildLink}
+                                        href="/"
+                                      >
+                                        {itemSubChild.name}
+                                      </a>
+                                    </li>
+                                  ))}
+                                </ul>
+                              </li>
+                            )}
+                          </>
                         ))}
                         <li className={styles.subChildItem}>
                           <a
@@ -202,7 +218,11 @@ const HeaderSubNav = ({
                             }}
                             style={{ color: '#f04950' }}
                           >
-                            {parseText(cookies, 'Все категории', 'Всі категорії')}
+                            {parseText(
+                              cookies,
+                              'Все категории',
+                              'Всі категорії',
+                            )}
                           </a>
                         </li>
                       </ul>
