219704317918a081b5c33ec5e555b2fb8e0ae0d4
SamborVadym <samborvadym@gmail.com>
2021-01-12 12:39:14 +0200

    fixed

--- utils/validation.js
+++ utils/validation.js
@@ -1,20 +1,36 @@
+import { parseText } from './helpers';
+import { cookies } from './getCookies';
+
 export const required = (value) => {
   if (!value || value.length === 0) {
-    return 'поле обязательно к заполнению';
+    return parseText(
+      cookies,
+      'поле обязательно к заполнению',
+      `
+поле обов'язково до заповнення`,
+    );
   }
   return undefined;
 };
 
 export const emailValidation = (value) => {
   if (!/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(value)) {
-    return 'введите корректный e-mail';
+    return parseText(
+      cookies,
+      'введите корректный e-mail',
+      'введіть коректний e-mail',
+    );
   }
   return undefined;
 };
 
 export const snpValidation = (value) => {
   if (/\d+$/g.test(value)) {
-    return 'имя не должно содержать цифры';
+    return parseText(
+      cookies,
+      'имя не должно содержать цифры',
+      "ім'я не повинно містити цифри",
+    );
   }
 
   return undefined;
@@ -25,7 +41,7 @@ export const numberValidation = (value) => {
     !/\d+$/g.test(value)
     || !/^(\+38\ )\((0)\d{2}\)(\ )\d{3}(\ )\d{2}(\ )\d{2}$/.test(value)
   ) {
-    return 'номер не валиден';
+    return parseText(cookies, 'номер не валиден', 'номер не валідний');
   }
 
   return undefined;
@@ -33,7 +49,11 @@ export const numberValidation = (value) => {
 
 export const passwordValidation = (value) => {
   if (value.length < 8) {
-    return 'поле пароля не должно содержать меньше 8 символов';
+    return parseText(
+      cookies,
+      'поле пароля не должно содержать меньше 8 символов',
+      'поле пароля не повинно містити менше 8 символів',
+    );
   }
   return undefined;
 };
