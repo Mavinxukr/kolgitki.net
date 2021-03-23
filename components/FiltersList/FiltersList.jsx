57587b671b31c400e48d0a0f16c0d4cbac967927
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-03-23 15:37:03 +0200

    filters list styles

--- components/FiltersList/FiltersList.jsx
+++ components/FiltersList/FiltersList.jsx
@@ -4,7 +4,6 @@ import { parseText } from '../../utils/helpers';
 import FiltersItem from './FiltersItem/FiltersItem';
 import styles from './FiltersList.scss';
 
-//the component accepts an object of all filters (except for categories) [installedFilters] and the method to clean up this object [clearFilters]
 const FiltersList = ({
   installedFilters,
   clearFilters,
@@ -12,31 +11,34 @@ const FiltersList = ({
   removeOneFilter
 }) => {
   return (
-    <div className={styles.indicatorsButtons}>
-      {Object.keys(installedFilters).length > 0 && (
-        <button
-          className={styles.indicatorsDeleteButton}
-          onClick={() => {
-            clearFilters(Object.keys(installedFilters));
-          }}
-        >
-          {parseText(cookies, 'Удалить фильтры', 'Видалити фільтри')}
-        </button>
-      )}
-      {Object.keys(installedFilters).map(filter => {
-        return JSON.parse(installedFilters[filter]).map((item, index) => {
-          return (
-            <FiltersItem
-              key={index}
-              item={item}
-              removeOneFilter={() => {
-                removeOneFilter(filter, item);
-              }}
-            />
-          );
-        });
-      })}
-      <a onClick={getProductHandle} className={styles.setFilterButton}>
+    <div className={styles.filters}>
+      <div className={styles.filters_list}>
+        {Object.keys(installedFilters).length > 0 && (
+          <button
+            className={styles.indicatorsDeleteButton}
+            onClick={() => {
+              clearFilters(Object.keys(installedFilters));
+            }}
+          >
+            {parseText(cookies, 'Удалить фильтры', 'Видалити фільтри')}
+          </button>
+        )}
+        {Object.keys(installedFilters).map(filter => {
+          return JSON.parse(installedFilters[filter]).map((item, index) => {
+            return (
+              <FiltersItem
+                key={index}
+                item={item}
+                removeOneFilter={() => {
+                  removeOneFilter(filter, item);
+                }}
+              />
+            );
+          });
+        })}
+      </div>
+      <div className={styles.filters_button_block}></div>
+      <a onClick={getProductHandle} className={styles.filters_button_apply}>
         {parseText(cookies, 'Применить', 'Застосувати')}
       </a>
     </div>
