05c64b23d1b60258a1880e909298f62090660265
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-03-30 17:15:13 +0300

    fix bag hover

--- components/Layout/Header/BottomHeader/MenuItem/MenuItem.jsx
+++ components/Layout/Header/BottomHeader/MenuItem/MenuItem.jsx
@@ -9,30 +9,22 @@ export const MenuItem = ({ menuItem }) => {
   const [imageLink, setImageLink] = useState(null);
   const [visible, setVisible] = useState(false);
   const [contentSubMenu, setContentSubMenu] = useState([]);
-  const [classes, setClasses] = useState([styles.item_link]);
   return (
     <li
       className={styles.item}
       onMouseEnter={ev => {
         setVisible(true);
         setImageLink(menuItem.image_link);
-        if (
-          ev.target.innerText ===
-          parseText(cookies, menuItem.name, menuItem.name_ua)
-        ) {
-          setClasses(prev => [...prev, styles.active]);
-        }
       }}
       onMouseLeave={() => {
         setVisible(false);
         setImageLink(null);
-        setClasses(prev => prev.filter(item => item !== styles.active));
       }}
     >
       {/* <Link href={`/products/${menuItem.slug}`}> */}
       <a
         href={`/products/${menuItem.slug}`}
-        className={classes.join(' ')}
+        className={styles.item_link}
         onMouseEnter={ev => {
           if (
             ev.target.innerText ===
