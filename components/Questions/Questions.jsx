833788d89c2e690ea2751896f2377380016b7cea
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-05-27 13:52:09 +0300

    question styles

--- components/Questions/Questions.jsx
+++ components/Questions/Questions.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Button from '../Layout/Button/Button';
 import { cookies } from '../../utils/getCookies';
 import { parseText } from '../../utils/helpers';
 import styles from './Questions.scss';
-
-const DynamicComponentWithNoSSRAccordion = dynamic(
-  () => import('../Accordion/Accordion'),
-  { ssr: false },
-);
+import { ReactAccordion } from '../ReactAccordion/ReactAccordion';
 
 const DropDownItem = ({ answer, answerUa }) => (
   <div className={styles.info}>
-    <div className={styles.infoDesc}
+    <div
+      className={styles.infoDesc}
       dangerouslySetInnerHTML={{ __html: parseText(cookies, answer, answerUa) }}
     />
+
     <Link href="/about/pick-up-points" prefetch={false}>
       <Button
         buttonType="button"
@@ -33,7 +30,7 @@ const DropDownItem = ({ answer, answerUa }) => (
 const Questions = ({ questions, classNameWrapper }) => {
   const [indexActive, setIndexActive] = useState(0);
 
-  const onSetIndexAccordion = (id) => {
+  const onSetIndexAccordion = id => {
     if (indexActive === id) {
       setIndexActive(0);
     } else {
@@ -41,39 +38,44 @@ const Questions = ({ questions, classNameWrapper }) => {
     }
   };
 
+  const accordionContent = questions.map(item => {
+    return {
+      uuid: item.id.toString(),
+      heading: (
+        <div className={styles.headerWrapper}>
+          {parseText(cookies, item.question, item.question_ua)}
+        </div>
+      ),
+      content: <DropDownItem answer={item.answer} answerUa={item.answer_ua} />
+    };
+  });
+
   return (
     <div className={classNameWrapper}>
       <h3 className={styles.title}>
         {parseText(cookies, 'Вопросы и Ответы', 'Питання та відповіді')}
       </h3>
-      <ul className={styles.accordion} uk-accordion="multiple: true">
-        {questions.map(item => (
-          <DynamicComponentWithNoSSRAccordion
-            key={item.id}
-            classNameWrapper={styles.item}
-            addClassNameWrapper={styles.itemOpen}
-            title={item.question}
-            titleUk={item.question_ua}
-            toggled={false}
-            setIndexActive={() => onSetIndexAccordion(item.id)}
-            isCurrentAccordionActive={indexActive === item.id}
-          >
-            <DropDownItem answer={item.answer} answerUa={item.answer_ua} />
-          </DynamicComponentWithNoSSRAccordion>
-        ))}
-      </ul>
+      <ReactAccordion
+        allowZeroExpanded={true}
+        accordionClasses={styles.accordion}
+        accordionItemClasses={styles.accordion_item}
+        accordionHeadingClasses={styles.accordion_header}
+        accordionButtonClasses={styles.accordion_button}
+        accordionPanelClasses={styles.accordion_panel}
+        items={accordionContent}
+      />
     </div>
   );
 };
 
 Questions.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.object),
-  classNameWrapper: PropTypes.string,
+  classNameWrapper: PropTypes.string
 };
 
 DropDownItem.propTypes = {
   answer: PropTypes.string,
-  answerUa: PropTypes.string,
+  answerUa: PropTypes.string
 };
 
 export default Questions;
