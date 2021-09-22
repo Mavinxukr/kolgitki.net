78178fc3dd0b78b091831486714862bb515c7c22
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-09-22 15:00:21 +0300

    touch none

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -7,9 +7,13 @@ import { useEffect } from 'react';
 const Popup = ({ children, classNameWrapper, closePopup }) => {
   useEffect(() => {
     document.body.style.overflow = 'hidden';
+    document.body.style.touchAction = 'none';
+    document.body.style.msTouchAction = 'none';
 
     return () => {
       document.body.style.overflow = 'auto';
+      document.body.style.touchAction = 'auto';
+      document.body.style.msTouchAction = 'auto';
     };
   });
 
