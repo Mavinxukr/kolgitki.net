2dc234fe63068d8b9be013278daa375699994153
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-09-22 13:40:01 +0300

    fix scroll popup

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './Popup.scss';
+import { useEffect } from 'react';
 
-const Popup = ({
-  children, classNameWrapper, closePopup,
-}) => (
-  <div className={styles.backgroundPopup}>
-    <div
-      className={styles.popupOverlay}
-      onClick={() => closePopup()}
-      onKeyPress={closePopup}
-      role="button"
-      tabIndex="0"
-      aria-label="Mute volume"
-    />
-    <div className={cx(styles.popup, classNameWrapper)}>
-      {children}
+const Popup = ({ children, classNameWrapper, closePopup }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  });
+
+  return (
+    <div className={styles.backgroundPopup}>
+      <div
+        className={styles.popupOverlay}
+        onClick={() => closePopup()}
+        onKeyPress={closePopup}
+        role="button"
+        tabIndex="0"
+        aria-label="Mute volume"
+      />
+      <div className={cx(styles.popup, classNameWrapper)}>{children}</div>
     </div>
-  </div>
-);
+  );
+};
 
 Popup.propTypes = {
   children: PropTypes.node,
   classNameWrapper: PropTypes.string,
-  closePopup: PropTypes.func,
+  closePopup: PropTypes.func
 };
 
 export default Popup;
